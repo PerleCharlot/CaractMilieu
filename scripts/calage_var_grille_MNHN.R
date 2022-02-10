8de26e9d7c9dd8ad4a72a1bf747229b786867f1e
Perle Charlot <perle.charlot@inrae.fr>
2022-02-10 09:48:53 +0100

    amélioration nommage et gestion dossiers

--- scripts/calage_var_grille_MNHN.R
+++ scripts/calage_var_grille_MNHN.R
@@ -10,16 +10,29 @@ library(raster)
 
 ### Fonctions -------------------------------------
 
+# Fonction qui permet de lire un raster, le rééchantilloner selon une référence et l'enregistrer
+recalageRaster <- function(r, grille_raster_reference, path_dos_sauv){
+  r <- raster(r)
+  fn <- paste0(path_dos_sauv,"/",names(r),"_cale.tif")
+  r_cale <- resample(r, grille_raster_reference, filename=fn, progress="text")
+}
+
 ### Constantes -------------------------------------
 
 # Espace de travail
-wd <- "C:/Users/perle.charlot/Documents/PhD/DATA/"
+wd <- getwd()
+# Dossier des inputs (dans le git)
+input_path <- paste0(wd,"/input/")
+# Dossier des variables spatiales
+dos_var_sp <- "C:/Users/perle.charlot/Documents/PhD/DATA/Variables_spatiales_Belledonne/"
 
-# Dossier où l'on souhaite sauvegarder les variables calculées
-dossier_sauv <- paste0(wd,"/Variables_spatiales_Belledonne/Milieux/variables_calculees_mnt/")
+# Dossier de sauvegarde des couches finales (avant recalage)
+dos_sauv_1 <- paste0(dos_var_sp,"COUCHES_MILIEU_AVANT_RECALAGE")
+# Dossier de sauvegarde des couches finales (après recalage)
+dos_sauv_2 <- paste0(dos_var_sp,"COUCHES_MILIEU_FINALES")
 
 # Chemin de la grille de référence
-path_grill_ref <- paste0(wd,"Variables_spatiales_Belledonne/zonage_MNHN/zonage_raster_25m.TIF")
+path_grill_ref <- paste0(dos_var_sp,"/zonage_MNHN/zonage_raster_25m.TIF")
 ### Programme -------------------------------------
 
 # Espace de travail
@@ -29,10 +42,16 @@ setwd(wd)
 grille_calee <- raster(path_grill_ref)
 
 # Chargement des raster à recaler
-TPi <- raster(paste0(dossier_sauv,"TPI_25m.tif"))
+LR <- list.files(dos_sauv_1,full.names = TRUE)
 
-# Recalage des variables selon la grille
-TPI_cale <- resample(TPi, grille_calee)
 
-# SAuvegarde des rasters recalés
-writeRaster(TPI_cale,paste0(dossier_sauv,"TPI_25m_cale.tif"))
+# Utilisatinon recalage pour une liste de rasters
+lapply(LR, recalageRaster,
+       grille_raster_reference = grille_calee,
+       path_dos_sauv = dos_sauv_2)
+
+recalageRaster(paste0(dos_var_sp,"Milieux/IGN/mnt_25m_belledonne.tif"),
+               grille_raster_reference = grille_calee,
+               path_dos_sauv = dos_sauv_2)
+
+
