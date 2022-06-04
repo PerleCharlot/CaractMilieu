606ce5b5fe3dc1950e8d7ebc08ed747da4567fb4
Perle Charlot <perle.charlot@inrae.fr>
2022-06-04 11:26:49 +0200

    création script aggrégation des variables par dimension et par saison

---
+++ scripts/aggrégation_par_dim.R
@@ -0,0 +1,96 @@
+### Titre -------------------------------------
+# Nom : Regroupement des variables à travers chaque dimension
+# Auteure : Perle Charlot
+# Date de création : 31-03-2022
+# Dates de modification : 04-06-2022
+
+### Librairies -------------------------------------
+
+library(raster)
+
+### Fonctions -------------------------------------
+
+# Fonction qui vérifie que le raster ait le bon CRS et extent, et le modifie si besoin
+AjustExtCRS <- function(path.raster.to.check, path.raster.ref=chemin_mnt){
+  
+  # # TEST
+  # path.raster.to.check = r_hiv[2]
+  # raster.ref = MNT
+  
+  raster.to.check <- raster(path.raster.to.check)
+  raster.ref <- raster(path.raster.ref)
+  
+  ext.to.check <- extent(raster.to.check)
+  bon.extent <- extent( raster.ref)
+  
+  sameCRS <- compareCRS(raster.to.check,EPSG_2154)
+  sameExtent <- (ext.to.check == bon.extent)
+  
+  if(any(!sameCRS,!sameExtent)) {
+    raster.to.check <- projectRaster(raster.to.check, raster.ref)
+    writeRaster(raster.to.check, path.raster.to.check, overwrite=TRUE)
+    cat("\nRaster ", names(raster.to.check)," a été modifié et sauvegarde.")
+  }
+  
+}
+
+# Fonction qui crée une stack de variables par dimension et par saison
+CreateStackDimSeason <- function(nom_dim){
+  
+  # # #TEST
+  # nom_dim = "D"
+  
+  nom_dim = as.character(nom_dim)
+  
+  liste_rasters <- list.files(paste0(output_path,"/var_",nom_dim,"/"),'.tif$',full.names = TRUE)
+  if(length(liste_rasters) == 0){
+    liste_rasters <- list.files(paste0(output_path,"/var_",nom_dim,"/"),'.TIF$',full.names = TRUE)
+  }
+    
+  # S'assurer de la conformité des variables
+  lapply(liste_rasters, AjustExtCRS)
+  
+  # Séparer hiver/été si existent
+  r_hiv <- liste_rasters[grep("hiv",liste_rasters)]
+  r_ete <- liste_rasters[grep("ete|été",liste_rasters)]
+  r_both <- liste_rasters[which(!liste_rasters %in% r_hiv & !liste_rasters %in% r_ete)]
+  
+  # Condition  
+  if(!sum(length(r_hiv), length(r_ete), length(r_both)) == length(liste_rasters)){
+    stop("\nProblème entre été et hiver.")
+  }
+  
+  # Création de la stack par saison
+  stack_hiv <- stack(c(r_hiv, r_both))
+  stack_ete <- stack(c(r_ete, r_both))
+  # Sauvegarde de la stack par saison par dimension
+  writeRaster(stack_hiv, paste0(output_path,"/stack_dim/",nom_dim,".tif"), overwrite=TRUE)
+  cat("\nDimension ", nom_dim, "terminée.")
+  
+}
+
+### Constantes -------------------------------------
+
+# Espace de travail
+wd <- getwd()
+# Dossier des inputs (dans le git)
+input_path <- paste0(wd,"/input/")
+# Dossier des outputs (dans le git)
+output_path <- paste0(wd,"/output/")
+# Projection Lambert 93 (EPSG : 2154)
+EPSG_2154 =  "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +units=m +no_defs "
+
+#### Données spatiales ####
+
+# Dossier des variables spatiales & chemins des fichiers
+dos_var_sp <- "C:/Users/perle.charlot/Documents/PhD/DATA/Variables_spatiales_Belledonne/"
+# MNT 25m CALé sur le grille de REFERENCE
+chemin_mnt <- paste0(dos_var_sp ,"/Milieux/IGN/mnt_25m_belledonne_cale.tif")
+
+#### Tables ####
+
+### Programme -------------------------------------
+
+liste_dimensions =  c("CA","B","PV","CS","D","I")
+
+lapply(liste_dimensions,CreateStackDimSeason)
